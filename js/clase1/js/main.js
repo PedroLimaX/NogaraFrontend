0c7128e3fe3e3baba6ae7bc9a3649c6dea1065dd
PedroLimaX <pedro99slip@gmail.com>
2022-03-08 21:36:13 -0600

    js clase 1

---
+++ js/clase1/js/main.js
@@ -0,0 +1,88 @@
+var edad = 20;
+
+var nombre;
+
+nombre = 90;
+
+
+var fondos = 100;
+
+fondos = 150;
+
+fondos = 120;
+
+fondos = -100;
+
+var nombre = 'Pedro';
+
+var wa = nombre;
+var fb=nombre;
+var ig=nombre;
+var tw = nombre;
+
+
+// console.log(nombre);
+// console.log(wa);
+// console.log(fb);
+// console.log(ig);
+// console.log(tw);
+
+// var casado = false;
+// var muerto = false;
+// if (casado) {
+//   console.log('F');
+// }
+
+
+// var pregunta=prompt('Cual es tu nombre?');
+
+// console.log('Tu nombre es:' + nombre);
+
+// var confirmation = confirm('¿Juegas LOL?');
+
+// if(confirmation){
+//   console.log('Bañese');
+// }
+// else{
+//   console.log('Felicidades');
+// }
+
+var num1 = 198;
+var num2 = 61;
+var num3 = -26151;
+
+var suma = num1 + num2;
+var resta = num1 - num2;
+var division = num1 / num2;
+var multi = num1 * num2;
+
+var mod = num1 % num2;
+
+
+
+// console.log(mod);
+
+var dinero = 300;
+var pcgamer = 800;
+
+// if(dinero > pcgamer){
+//   console.log('Si te alcanza');
+// }
+// if(dinero < pcgamer){
+//   console.log('No te alcanza');
+// }
+// if(dinero == pcgamer){
+//   console.log('Apenas y te alcanza');
+// }
+
+var mayoriaEdad = prompt('¿Que edad tienes?');
+
+if(mayoriaEdad < 18){
+  console.log('No puedes acceder al bar');
+}
+if (mayoriaEdad >18) {
+  console.log('Puedes entrar al bar');
+}
+if(mayoriaEdad==18){
+  console.log('Ya puedes entrar al bar');
+}
